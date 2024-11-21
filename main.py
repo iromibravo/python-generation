4734ac4afd9db607d1b68e43bd0b95d8c78e46d7
iromibravo <1111vanessabravo@gmail.com>
2024-11-20 21:13:13 -0500

    Add file

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write a program that takes two input numbers, then adds them together and prints the result.
+num1 = float(input("Enter an number: "))
+num2 = float(input("Enter an number: "))
+
+# Sumar los números
+result = num1 + num2
+
+# Imprimir el resultado
+print("Result:", result)
